fbab3695a13d838ba997265b6ce8cae1f60b304f
dbgb <dbayliss.mail@gmail.com>
2020-08-18 11:54:30 +0100

    test: set up Jest unit testing environment

---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Header from "../Header";
+
+test("This is a test test!", () => {
+  expect(true).toBe(true);
+});
