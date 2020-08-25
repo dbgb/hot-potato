14f44a869f93b29273784ba1bb8082f51a401c9d
dbgb <dbayliss.mail@gmail.com>
2020-08-25 12:56:27 +0100

    fix: add aria label to Sidebar handle

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from "react";
 import "./Sidebar.css";
 
 export default function Sidebar({ width, height = "100vh", children }) {
-  const [xPos, setXPos] = useState(0);
+  const [xPos, setXPos] = useState(-width);
 
   const handleToggle = () => {
     if (xPos < 0) {
@@ -24,6 +24,7 @@ export default function Sidebar({ width, height = "100vh", children }) {
       >
         <button
           className="sidebarToggle"
+          aria-label="Toggle sidebar"
           onClick={() => handleToggle()}
           style={{ transform: `translate(${width}px, 25vh)` }}
         ></button>
