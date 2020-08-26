717101431e654c1a63470141278dd660c53c741b
dbgb <dbayliss.mail@gmail.com>
2020-08-26 19:59:00 +0100

    test(a11y): add logger to a11y test suite

--- cypress/e2e/a11y.js
+++ cypress/e2e/a11y.js
@@ -4,16 +4,44 @@ describe("Accessibility tests", () => {
   });
 
   it("Home page: has no critical impact accessibility violations", () => {
-    cy.checkA11y(null, {
-      includedImpacts: ["critical"],
-    });
+    cy.checkA11y(
+      null,
+      {
+        includedImpacts: ["critical"],
+      },
+      terminalLog
+    );
   });
   it("Recipes page: has no critical impact accessibility violations", () => {
     cy.get("a[href*='recipes']")
       .findByText(/recipes/i)
       .click()
-      .checkA11y(null, {
-        includedImpacts: ["critical"],
-      });
+      .checkA11y(
+        null,
+        {
+          includedImpacts: ["critical"],
+        },
+        terminalLog
+      );
   });
 });
+
+function terminalLog(violations) {
+  cy.task(
+    "log",
+    `${violations.length} accessibility violation${
+      violations.length === 1 ? "" : "s"
+    } ${violations.length === 1 ? "was" : "were"} detected`
+  );
+  // pluck specific keys to keep the table readable
+  const violationData = violations.map(
+    ({ id, impact, description, nodes }) => ({
+      id,
+      impact,
+      description,
+      nodes: nodes.length,
+    })
+  );
+
+  cy.task("log", violationData);
+}
