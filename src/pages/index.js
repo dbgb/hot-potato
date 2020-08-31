2c896daf525c8cb1bd445c270ddc163e760dda1c
dbgb <dbayliss.mail@gmail.com>
2020-08-31 12:33:23 +0100

    feat: update site entrance page with new app logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,21 @@
 import React from "react";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
 import Layout from "../components/Layout";
 import SEO from "../components/Seo";
 
 const IndexPage = ({ data }) => {
   const { title, description } = data.site.siteMetadata;
+  const { fixed: logo } = data.file.childImageSharp;
 
   return (
     <Layout>
       <SEO title={title} />
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <p>
-        Aperiam quisquam sed quia molestias nisi sint est quidem corporis.
-        Consectetur non consectetur maiores. Qui fugiat voluptatem voluptate
-        perferendis eaque. Accusamus sit et consequuntur ad. Qui alias itaque
-        libero similique numquam.
-      </p>
-      <p>
-        Eum repellendus distinctio itaque quas. Voluptatibus delectus numquam
-        est repudiandae mollitia id aut ut aut. Eos laborum consectetur iure cum
-        corporis. Qui et qui ut doloremque cupiditate autem sint eligendi.
-        Consectetur velit eos non ratione aut. Sunt alias in accusantium sed et
-        eos est. Qui nam esse cumque sed aut quia accusantium. Sint quisquam ea
-        ea temporibus sapiente voluptates voluptatem itaque.
-      </p>
-      <Link to="/using-typescript/">"Using TypeScript in Gatsby"</Link>
+      <div style={{ textAlign: "center" }}>
+        <h1>{title}</h1>
+        <Img fixed={logo} style={{ display: "block", margin: "0 auto" }} />
+        <p>{description}</p>
+      </div>
     </Layout>
   );
 };
@@ -36,6 +26,13 @@ export const pageQuery = graphql`
       siteMetadata {
         title
         description
+      }
+    }
+    file(relativePath: { eq: "hot-potato.png" }) {
+      childImageSharp {
+        fixed(width: 350, height: 350) {
+          ...GatsbyImageSharpFixed_tracedSVG
+        }
       }
     }
   }
