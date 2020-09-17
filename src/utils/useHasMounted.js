8f7d1beb18c0e84ad40396a7adefd8e76b844dfe
dbgb <dbayliss.mail@gmail.com>
2020-09-17 16:43:48 +0100

    feat: add hook for detecting component mount state

---
+++ src/utils/useHasMounted.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+/**
+ * React hook to provide an accurate indicator of whether a component has
+ * mounted.
+ *
+ * `hasMounted` can be used as accurate indicator of whether browser
+ * API's are available (component is mounted), or browser API's are not
+ * available (DOM is being built as a part of SSR, or currently in a
+ * pre-hydrated state).
+ */
+function useHasMounted() {
+  // defaults to false, and latches to true when mounted.
+  const [hasMounted, setHasMounted] = useState(false);
+
+  // `useEffect` only fires if the component is mounted.
+  // Since this side effect doesn't depend on any props/state values, passing an
+  // empty deps array tells React that this effect only needs to run once, when
+  // it is first triggered.
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  return hasMounted;
+}
+
+export default useHasMounted;
