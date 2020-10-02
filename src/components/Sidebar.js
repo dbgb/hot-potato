f0fc606c9c71beef90678122459ef3b6f3f95c88
dbgb <dbayliss.mail@gmail.com>
2020-10-02 13:16:42 +0100

    refactor(sidebar): decouple props based styling from component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,50 +2,56 @@ import React, { useState, useContext } from "react";
 import styled from "styled-components";
 import { ThemeContext } from "../styles/ThemeContext";
 
+const SidebarContainer = styled.div`
+  position: fixed;
+  top: 0;
+  min-height: 100vh;
+  width: 350px;
+  border-right: 1px solid burlywood;
+  background-color: ${(props) => props.bg};
+  opacity: 1;
+  z-index: 1;
+  transition: 0.3s ease;
+  transform: ${(props) =>
+    props.isOpen ? `translate(${props.position}px)` : `translate(0px)`};
+
+  /* TODO: SSR friendly sidebar state detection / behaviour
+  | 
+  | First paint: SSR friendly CSS media query sets sidebar position
+  | After hydration: once mounted/set, isOpen state dictates sidebar position
+  |
+  | "hasMounted ? null : -width"
+
+  @media screen and (max-width: 767px) {
+    transform: translate(-350px);
+  }
+  @media screen and (min-width: 768px) {
+    transform: translate(0px);
+  }
+  */
+`;
+
 const SidebarContent = styled.div``;
 
+const SidebarToggle = styled.button`
+  position: absolute;
+  top: 25vh;
+  height: 100px;
+  border: 1px solid #373737;
+  border-left: 0;
+  border-top-right-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  background-color: ${(props) => props.bg};
+  outline: none;
+  z-index: 1;
+  transform: ${(props) => `translate(${props.position}px)`};
+`;
+
 const Sidebar = ({ width = 350, children }) => {
-  // First paint: SSR friendly CSS media query sets sidebar position
-  // After hydration: once `isOpen` state is set, it dictates sidebar position
-  const [xPos, setXPos] = useState(-width); // closed by default (mobile friendly)
-  const [isOpen, setIsOpen] = useState(xPos === 0); // true if sidebar open
   const theme = useContext(ThemeContext);
 
-  const SidebarContainer = styled.div`
-    position: fixed;
-    top: 0;
-    min-height: 100vh;
-    width: 350px;
-    background-color: ${theme.secondary};
-    border-right: 1px solid burlywood;
-    opacity: 1;
-    z-index: 1;
-    transition: 0.3s ease;
-
-    ${"" /* transform: translate(${xPos}px); */}
-    ${"" /* transform: ${isOpen ? translate(Xpx) : translate(Ypx)} */}
-
-    @media screen and (max-width: 767px) {
-      transform: translate(-350px);
-    }
-    @media screen and (min-width: 768px) {
-      transform: translate(0px);
-    }
-  `;
-
-  const SidebarToggle = styled.button`
-    position: absolute;
-    top: 25vh;
-    height: 100px;
-    border: 1px solid #373737;
-    border-left: 0;
-    border-top-right-radius: 1rem;
-    border-bottom-right-radius: 1rem;
-    background-color: ${theme.primary};
-    outline: none;
-    z-index: 1;
-    transform: translate(350px);
-  `;
+  const [xPos, setXPos] = useState(-width); // closed by default (mobile friendly)
+  const [isOpen, setIsOpen] = useState(xPos === 0); // true if sidebar open
 
   const handleToggle = () => {
     if (isOpen) {
@@ -58,13 +64,12 @@ const Sidebar = ({ width = 350, children }) => {
   };
 
   return (
-    <SidebarContainer style={{ transform: `translate(${xPos}px)` }}>
+    <SidebarContainer bg={theme.secondary} position={xPos} isOpen>
       <SidebarToggle
+        bg={theme.primary}
+        position={width}
         onClick={() => handleToggle()}
         aria-label="Toggle sidebar"
-        style={{
-          backgroundColor: theme.primary,
-        }}
       />
       <SidebarContent>{children}</SidebarContent>
     </SidebarContainer>
