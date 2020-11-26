6272686e00fdcb53f604e16c99bb2f0b977612ab
dbgb <dbayliss.mail@gmail.com>
2020-11-26 14:11:53 +0000

    test: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,22 @@
 module.exports = {
   // ref: https://www.gatsbyjs.com/docs/unit-testing/
   // ref: https://jestjs.io/docs/en/configuration.html
-  transform: {
-    "^.+\\.jsx?$": "<rootDir>/jest/custom-transformer.js",
+  globals: {
+    __PATH_PREFIX__: "",
   },
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/jest/__mocks__/file-mock.js",
+      "<rootDir>/jest/__mocks__/file-stub.js",
   },
-  testPathIgnorePatterns: ["node_modules", ".cache", "<rootDir>.*/public"],
-  watchPathIgnorePatterns: [".cache"],
-  transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
-  globals: {
-    __PATH_PREFIX__: "",
-  },
-  testURL: "http://localhost",
+  roots: ["<rootDir>", "<rootDir>/jest"],
   setupFiles: ["<rootDir>/jest/gatsby-loader-shim.js"],
   setupFilesAfterEnv: ["<rootDir>/jest/post-env-setup.js"],
+  transform: {
+    "^.+\\.jsx?$": "<rootDir>/jest/custom-transformer.js",
+  },
+  testPathIgnorePatterns: ["node_modules", ".cache", "<rootDir>.*/public"],
+  transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
+  watchPathIgnorePatterns: [".cache"],
+  verbose: true,
 };
