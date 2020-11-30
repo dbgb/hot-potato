04698f166f874987fee3873fb4dd2d0dcc8817bd
dbgb <dbayliss.mail@gmail.com>
2020-11-30 19:43:14 +0000

    test(cypress): tweak e2e smoke tests

--- cypress/e2e/smoke.js
+++ cypress/e2e/smoke.js
@@ -1,13 +1,17 @@
-describe("Smoke tests", () => {
-  it("Home page: renders title", () => {
+describe("Home page: smoke tests", () => {
+  it("Renders the header title", () => {
     cy.visit("/");
     cy.get("header").findByText(/hot potato!/i);
   });
-  it("Home page: navigates to the recipes page", () => {
+
+  it("Navigates to the recipes page", () => {
     cy.visit("/");
     cy.get("a[href*='recipes']").click();
   });
-  it("Recipes page: renders title", () => {
+});
+
+describe("Recipe index: smoke tests", () => {
+  it("Renders the header title", () => {
     cy.visit("/recipes");
     cy.get("main > h1").findByText(/recipe index/i);
   });
