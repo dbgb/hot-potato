bf95d04ff902628696e446a42b8b3336d609ae8b
dbgb <dbayliss.mail@gmail.com>
2020-12-28 02:14:29 +0000

    test(jest): update Header tests

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -3,17 +3,43 @@ import { render, screen } from "@testing-library/react";
 import { useStaticQuery } from "gatsby";
 import Header from "../Header";
 
+// stub matchMedia as it is not currently implemented in JSDOM
+// ref: https://stackoverflow.com/questions/39830580/
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 beforeEach(() => {
   useStaticQuery.mockImplementationOnce(() => {
     // Mocked from GraphQL query return data
     return {
-      file: {
+      standardLogo: {
         childImageSharp: {
           fixed: {
-            src: "test-source",
-            srcSet: "test-source-set",
+            src: "test-source-standard",
+            srcSet: "test-set-standard",
             height: 50,
             width: 50,
+          },
+        },
+      },
+      tinyLogo: {
+        childImageSharp: {
+          fixed: {
+            src: "test-source-tiny",
+            srcSet: "test-source-set-tiny",
+            height: 36,
+            width: 36,
           },
         },
       },
@@ -30,7 +56,8 @@ test("Renders the title, logo and homepage link by default", () => {
   expect(headerLink).toHaveTextContent(testTitle);
 
   expect(screen.getByRole("heading", testTitle)).toBeInTheDocument();
-  expect(screen.getByRole("img")).toHaveAttribute("src", "test-source");
+  // Tiny image is the default where window object is not defined eg. in SSR
+  expect(screen.getByRole("img")).toHaveAttribute("src", "test-source-tiny");
 });
 
 test.todo("Header link and logo are not visible if spaceOnly prop is true");
