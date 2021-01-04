09adf84901ea6654d7f8e5934c09e0fd3ff638b7
dbgb <dbayliss.mail@gmail.com>
2021-01-04 10:09:19 +0000

    style: remove errant whitespace in css variable definition

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const GlobalStyles = createGlobalStyle`
     /* Global CSS Variables */
     --ease: 0.3s ease;
     --max-width-main: 61rem;
-    --offset-header-bottom : 5rem;
+    --offset-header-bottom: 5rem;
     --offset-content-top: calc(var(--offset-header-bottom) + 2rem);
 
     /* Global CSS Styling */
