fb888401240401ba14f1a8d5cb059f8a2eba6085
dbgb <dbayliss.mail@gmail.com>
2021-01-19 17:53:25 +0000

    build: keep recipes dir and index in sync with source files

--- src/utils/build_recipes.sh
+++ src/utils/build_recipes.sh
@@ -6,7 +6,11 @@
 # - - - - - - - - - - - - - - -
 
 RECIPES_SRC="/c/projects/recipes"
-RECIPES_DEST="../recipes/"
+RECIPES_DEST="../recipes"
+
+# Keep recipes in sync with source
+mkdir -p $RECIPES_DEST
+rm $RECIPES_DEST/*.markdown
 
 # Copy recipes from source to local app recipe folder (retain date created)
 printf "%s\n" "Copying recipes from $RECIPES_SRC to $RECIPES_DEST..."
@@ -15,4 +19,5 @@ cp -v "$RECIPES_SRC"/*.markdown "$RECIPES_DEST"
 # Pipe null terminated stream of recipe names to frontmatter injection script
 find "$RECIPES_DEST" -type f -name "*.markdown" -print0 | xargs -0 -I recipe ./prepend_frontmatter.sh recipe
 
-printf "%s\n" "Recipe files updated!"
+printf "\n%s\n\n" "Recipe files updated."
+printf "%s\n\n" "INFO: Run 'yarn build' to update the search index!"
